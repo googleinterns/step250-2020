2361eff106c1dc64bb989cffc11195115b65d1d5
Nikita Tsarev <tsarn@google.com>
2020-08-14 12:12:49 +0300

    A basic prototype of the groups page

---
+++ coffee-chats/src/main/webapp/src/components/GroupListPage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Box, Container, Grid, TextField, Typography} from "@material-ui/core";
+import {GroupCard} from "./GroupCard";
+
+export function GroupListPage() {
+  return (
+      <Box mt={4}>
+        <Container maxWidth="md">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                  fullWidth
+                  variant="outlined"
+                  label="Search your groups"/>
+            </Grid>
+            {["Board Games Dublin", "Book Club", "Mountain Climbers @ Home"].map(name =>
+                <Grid item xs={12}>
+                  <GroupCard name={name}/>
+                </Grid>
+            )}
+          </Grid>
+        </Container>
+      </Box>
+  );
+}
