c1ba2fa6f6f7e30f7b069f09a3532c542886c09e
Nikita Tsarev <tsarn@google.com>
2020-08-17 11:13:38 +0300

    Add a create group button

--- coffee-chats/src/main/webapp/src/components/GroupListPage.tsx
+++ coffee-chats/src/main/webapp/src/components/GroupListPage.tsx
@@ -1,20 +1,40 @@
 import React from "react";
-import {Box, Container, Grid, TextField} from "@material-ui/core";
+import {Box, Container, Fab, Grid, Icon, TextField} from "@material-ui/core";
 import {GroupCard} from "./GroupCard";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  extendedIcon: {
+    marginRight: theme.spacing(.5),
+  },
+  shrink: {
+    flexGrow: 0
+  }
+}));
 
 export function GroupListPage() {
+  const classes = useStyles();
+
   return (
       <Box mt={4}>
         <Container maxWidth="md">
           <Grid container spacing={2}>
-            <Grid item xs={12}>
+            <Grid item xs>
               <TextField
                   fullWidth
                   variant="outlined"
                   label="Search your groups"/>
             </Grid>
+            <Grid item xs className={classes.shrink}>
+              <Fab
+                  variant="extended"
+                  color="primary">
+                <Icon className={classes.extendedIcon}>add</Icon>
+                create
+              </Fab>
+            </Grid>
             {["Board Games Dublin", "Book Club", "Mountain Climbers @ Home"].map(name =>
-                <Grid item xs={12}>
+                <Grid item xs={12} key={name}>
                   <GroupCard name={name}/>
                 </Grid>
             )}
