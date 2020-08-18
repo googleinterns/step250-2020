1a46e41ccb3b2f164f8ab0ad708f5d7b32a11972
Nikita Tsarev <tsarn@google.com>
2020-08-18 14:22:40 +0300

    Add race detection to useFetch

--- coffee-chats/src/main/webapp/src/util/fetch.ts
+++ coffee-chats/src/main/webapp/src/util/fetch.ts
@@ -1,5 +1,25 @@
 import React from "react";
 
+interface ResponseRaceDetect {
+  response: Response;
+  json: any;
+  raceOccurred: boolean;
+}
+
+const fetchAndDetectRaces: (url: string) => Promise<ResponseRaceDetect> = (() => {
+  let requestId = 0;
+
+  return async (url: string) => {
+    const currentRequestId = ++requestId;
+
+    const response = await fetch(url);
+    const json = await response.json();
+    const raceOccurred = requestId != currentRequestId;
+
+    return {response, json, raceOccurred};
+  }
+})();
+
 /**
  * A hook that fetches JSON data from a URL using a GET request
  *
@@ -10,8 +30,11 @@ export function useFetch(url: string): any {
 
   React.useEffect(() => {
     (async () => {
-      const response = await fetch(url);
-      const json = await response.json();
+      const {response, json, raceOccurred} = await fetchAndDetectRaces(url);
+
+      if (raceOccurred) {
+        return;
+      }
 
       if (!response.ok && json.loginUrl) {
         window.location.href = json.loginUrl;
