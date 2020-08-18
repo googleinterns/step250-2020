1006db2254a7a22e62032a16eff4914896b038db
Nikita Tsarev <tsarn@google.com>
2020-08-18 11:45:18 +0300

    Make reverse proxy forward requests to AppEngine console as well

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -10,7 +10,7 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 http.createServer((req, res) => {
-    const regex = /^\/api/;
+    const regex = /^\/(api|_ah)/;
     const port = req.url.match(regex) ? backendDevServerPort : frontendDevServerPort;
     proxy.web(req, res, { target: `http://127.0.0.1:${port}` });
 }).listen(portThatProxyListensOn);
