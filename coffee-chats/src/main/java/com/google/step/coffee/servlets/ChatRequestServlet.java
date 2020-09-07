d7f1582f2a3a15d1db648e50323eea69c6e841f1
Prabhjot Grewal <prabsg@google.com>
2020-09-07 09:32:09 +0000

    Add error handling to ChatRequest servlet in case of missing or malformed parameters

--- coffee-chats/src/main/java/com/google/step/coffee/servlets/ChatRequestServlet.java
+++ coffee-chats/src/main/java/com/google/step/coffee/servlets/ChatRequestServlet.java
@@ -39,13 +39,11 @@ public class ChatRequestServlet extends JsonServlet {
   private ChatRequest buildChatRequestFromHttpRequest(HttpServletRequest request) throws HttpError {
     List<String> tags = getParameterValues("tags", request);
     List<String> dateStrings = getParameterValues("dates", request);
-    int minPeople = Integer.parseInt(getParameterOrDefault("minPeople", request, "1"));
-    int maxPeople = Integer.parseInt(getParameterOrDefault("maxPeople", request, "1"));
-    int durationMins = Integer.parseInt(getParameterOrDefault("durationMins", request, "30"));
-    boolean matchRandom =
-        Boolean.parseBoolean(getParameterOrDefault("matchRandom", request, "false"));
-    boolean matchRecents =
-        Boolean.parseBoolean(getParameterOrDefault("matchRecents", request, "true"));
+    int minPeople = parseIntParam("minPeople", request);
+    int maxPeople = parseIntParam("maxPeople", request);
+    int durationMins = parseIntParam("durationMins", request);
+    boolean matchRandom = parseBooleanParam("matchRandom", request, "false");
+    boolean matchRecents = parseBooleanParam("matchRecents", request, "true");
 
     List<Date> dates = dateStrings.stream()
         .map(Long::parseLong)
@@ -68,6 +66,15 @@ public class ChatRequestServlet extends JsonServlet {
     return (paramString != null) ? paramString : defaultValue;
   }
 
+  private String getParameter(String name, HttpServletRequest req) throws HttpError {
+    String paramString = req.getParameter(name);
+    if (paramString != null) {
+      return paramString;
+    } else {
+      throw new HttpError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameter: " + name);
+    }
+  }
+
   private List<String> getParameterValues(String name, HttpServletRequest req) {
     String paramString = req.getParameter(name);
     if (paramString == null || paramString.equals("")) {
@@ -75,5 +82,22 @@ public class ChatRequestServlet extends JsonServlet {
     }
 
     return Arrays.asList(paramString.split(","));
+  }
+
+  private Integer parseIntParam(String name, HttpServletRequest req) throws HttpError {
+    String paramString = getParameter(name, req);
+
+    try {
+      return Integer.parseInt(paramString);
+    } catch (NumberFormatException e) {
+      throw new HttpError(HttpServletResponse.SC_BAD_REQUEST,
+          "Invalid integer parameter: " + name);
+    }
+  }
+
+  private boolean parseBooleanParam(String name, HttpServletRequest req, String defaultValue) {
+    String paramString = getParameterOrDefault(name, req, defaultValue);
+
+    return Boolean.parseBoolean(paramString);
   }
 }
